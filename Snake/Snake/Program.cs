ad4c29a68a9e4ee9044a4ac3ad8528648a78ba61
AZ <hp550550@gmail.com>
2020-06-19 06:06:15 +0300

    function Drow()

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,8 +10,22 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int x1 = 1;
+            int y1 = 3;
+            char sym1 = '#';
+            Drow(x1, y1, sym1);
+
+
+
+
             Console.ReadLine();
         }
+
+        static void Drow(int x, int y, char sym)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        }
     }
+
 }
